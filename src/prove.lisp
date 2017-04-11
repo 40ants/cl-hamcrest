b91136cdd0a6d72c8492721e740200339ffcb726
Alexander Artemenko <svetlyak.40wt@gmail.com>
2017-04-11 22:41:51 +0300

    Removed hack around prove's reporter. Now cl-hamcrest depends on latest prove, which probably is not in the Quicklisp yet.

--- src/prove.lisp
+++ src/prove.lisp
@@ -16,36 +16,6 @@
 (cl-reexport:reexport-from 'hamcrest.matchers)
 
 
-(defvar original-report-expected-line
-  nil
-  "Holder for report-expected-line function, taken from prove.reporter.list.")
-
-
-(defvar rebind-report-expected-line
-  nil
-  "A flag. If it is true, then original report-expected-line
-should be replaced with new \"generic\" version.")
-
-(when (not (fboundp 'original-report-expected-line))
-  (setf (fdefinition 'original-report-expected-line)
-        #'prove.reporter.list::report-expected-line)
-  (setf rebind-report-expected-line t))
-
-
-(defgeneric report-expected-line (report)
-  (:documentation "Returns a line which describes a problem with failed test."))
-
-(defmethod report-expected-line (report)
-  "Default behaviour is to call original Prove's function,
-which formats a string, comparing two values."
-  (original-report-expected-line report))
-
-(when rebind-report-expected-line
-  (setf (fdefinition 'prove.reporter.list::report-expected-line)
-        #'report-expected-line)
-  (setf rebind-report-expected-line nil))
-
-
 (defclass assertion-report (prove.report:failed-test-report)
   ((prove.report:got :initform nil)
    (prove.report:expected :initform nil)
@@ -61,11 +31,14 @@ which formats a string, comparing two values."
                   :initform (error ":expected-line is required")
                   :reader expected-line)))
 
-(defmethod report-expected-line ((report assertion-report))
+
+(defmethod prove.reporter.list:report-expected-line ((report assertion-report))
   (expected-line report))
 
-(defmethod report-expected-line ((report passed-assertion-report))
+
+(defmethod prove.reporter.list:report-expected-line ((report passed-assertion-report))
   (expected-line report))
+
 
 (defmacro assert-that (value &rest matchers)
   "Main macro to test values agains matchers."
